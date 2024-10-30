60e341360d1b8154e9f2cace0169123cd1046cd4
AlexisVandeVreken <alexis.vandevreken@student.unamur.be>
2024-10-30 02:09:40 +0100

    Logos, dictionnary themes

--- app/gui.py
+++ app/gui.py
@@ -1,18 +1,41 @@
 from PyQt6 import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
+
+
+dark_theme = {
+    "bg_color": "#202020", #dark grey
+    "button color": "#444444",
+    "button_active_color": "#555555",
+    "font_color": "#ffffff",
+    "home_logo": "pictures\maison_white.png",
+    "tasks_logo": "pictures\todo_white.png",
+    "calendar_logo": "pictures\calendar_white.png",
+    "settings_logo": "pictures\settings_white.png",
+    "completed_logo": "pictures\done_white.png",
+    "menu_logo": "pictures\menu_white.png",
+    "add_logo": "pictures\add_white.png",
+    #add delete when tasks are implemented
+    "theme_logo": "pictures\sun.png",
+    "important_logo": "pictures\important_white.png"
+}
 
 light_theme = {
     "bg_color": "#f0f0f0", #light grey
     "button_color": "#ffffff", #white
     "tertiary_color": "#f0f0f0", 
     "font_color": "#000000", #black
-}
-dark_theme = {
-    "bg_color": "#202020", #dark grey
-    "button color": "#444444",
-    "button_active_color": "#555555",
-    "font_color": "#ffffff",
+    "home_logo": "pictures\maison.png",
+    "tasks_logo": "pictures\todo_black.png",
+    "calendar_logo": "pictures\calendar_black.png",
+    "settings_logo": "pictures\settings_black.png",
+    "completed_logo": "pictures\done_black.png",
+    "menu_logo": "pictures\menu_black.png",
+    "add_logo": "pictures\add_black.png",
+    #add delete when tasks are implemented
+    "theme_logo": "pictures\moon.png",
+    "important_logo": "pictures\important_black.png"
 }
 
 theme = light_theme
@@ -35,6 +58,7 @@ class MainWindow(QMainWindow):
         self.sidebar.setStyleSheet(f"background-color: {theme['tertiary_color']}")
         sidebar_layout = QVBoxLayout()
         self.sidebar.setLayout(sidebar_layout)
+        self.sidebar.minimumWidth = 55
         self.sidebar.setFixedWidth(200)
         self.sidebar.setContentsMargins(0, 0, 0, 0)
         main_layout.addWidget(self.sidebar)
@@ -47,21 +71,22 @@ class MainWindow(QMainWindow):
         main_layout.addWidget(self.main_content)
 
         #button to toggle sidebar, will be in sidebar container
-        self.toggle_sidebar_button = QPushButton(">")
+        self.toggle_sidebar_button = QPushButton(icon=QIcon("pictures\menu.png"), text="Collapse")
+        self.toggle_sidebar_button.setIcon(QIcon("pictures\menu.png"))
         self.toggle_sidebar_button.setStyleSheet(f"background-color: RED")
         self.toggle_sidebar_button.clicked.connect(self.toggle_sidebar)
         sidebar_layout.addWidget(self.toggle_sidebar_button)
 
         #sidebar buttons, will be in sidebar container
-        self.home_button = QPushButton("Home")
+        self.home_button = QPushButton(icon=QIcon("pictures\maison.png"), text="Home")
         self.home_button.setStyleSheet(f"background-color: {theme['button_color']}")
         sidebar_layout.addWidget(self.home_button)
 
-        self.tasks_button = QPushButton("Tasks")
+        self.tasks_button = QPushButton(icon=QIcon("pictures\liste.png"), text="Tasks")
         self.tasks_button.setStyleSheet(f"background-color: {theme['button_color']}")
         sidebar_layout.addWidget(self.tasks_button)
 
-        self.completed_button = QPushButton("Completed Tasks")
+        self.completed_button = QPushButton(icon=QIcon("pictures\liste.png"),text="Completed Tasks")
         self.completed_button.setStyleSheet(f"background-color: {theme['button_color']}")
         sidebar_layout.addWidget(self.completed_button)
 
@@ -82,6 +107,8 @@ class MainWindow(QMainWindow):
         #button to add task, will be in main content container
 
 
+        #check if sidebar is out or not
+        self.sidebar_out = True
 
     def set_theme(self, theme):
         #change the theme of the app
@@ -89,6 +116,21 @@ class MainWindow(QMainWindow):
 
     def toggle_sidebar(self):
         #hide or show the sidebar
+        if self.sidebar_out:
+            self.sidebar.setFixedWidth(50)
+            self.sidebar_out = False
+            for button in self.sidebar.children():
+                if isinstance(button, QPushButton):
+                    button.setText("")
+        elif self.sidebar_out == False:
+            self.sidebar.setFixedWidth(200)
+            self.sidebar_out = True
+            self.toggle_sidebar_button.setText("Collapse")
+            self.home_button.setText("Home")
+            self.tasks_button.setText("Tasks")
+            self.completed_button.setText("Completed Tasks")
+            self.calendar_button.setText("Calendar")
+            self.settings_button.setText("Settings")
         pass
 
         
